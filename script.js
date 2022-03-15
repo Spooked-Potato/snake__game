2cd4ef0e6380d73f17e93d62324bdd789d277362
Spooked-Potato <arturjddias@hotmail.com>
2022-03-15 23:15:39 +0000

    Full front Page

---
+++ script.js
@@ -0,0 +1,22 @@
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.title .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+
+anime.timeline({ loop: true })
+    .add({
+        targets: '.title .letter',
+        scale: [0, 1],
+        duration: 1500,
+        elasticity: 600,
+        delay: (el, i) => 45 * (i + 1)
+    })
+
+    .add({
+        targets: '.title',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
+
