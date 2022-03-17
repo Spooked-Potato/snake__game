0ebe829a19b881738faf90ba9e3905225028202a
Spooked-Potato <arturjddias@hotmail.com>
2022-03-17 00:23:03 +0000

    added overlay

--- JS/game.js
+++ JS/game.js
@@ -1,1 +1,11 @@
+alert("Hello world!");
+
+function on() {
+    document.getElementById("overlay").style.display = "block";
+    console.log("hey");
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
 
