f122d5fc67ca267afa3b5dc993a94fc94fc83316
Spooked-Potato <arturjddias@hotmail.com>
2022-03-23 20:24:02 +0000

    add grid working

--- JS/game.js
+++ JS/game.js
@@ -1,20 +1,35 @@
-const btn_on = document.queryselector(".btn__on");
-const btn_off = document.queryselector(".btn__off");
+const btn_on = document.querySelector(".button_top");
+const btn_off = document.querySelector(".btn__off");
 
-
-
-//alert("Hello world!");
-
-// function off() {
-//     document.getElementById("overlay").style.display = "none";
-// }
 
 btn_on.addEventListener("click", function () {
     document.getElementById("overlay").style.display = "block";
     console.log("hey");
 })
 
+var rows = 11;
+var columns = 11;
+
+gridContainer = document.getElementById('gridContainer');
+for (let rowNumber = 0; rowNumber < rows; rowNumber++) {
+
+    console.log("ola");
+    var row = document.createElement('div');
+    row.classList.add('row');
+    gridContainer.appendChild(row);
+
+    for (let colNumber = 0; colNumber < columns; colNumber++) {
+        var gridItem = document.createElement('div');
+        row.appendChild(gridItem);
+        gridItem.classList.add('gridItem');
+        gridItem.id = `${rowNumber}-${colNumber}`;
+    }
+}
+
 btn_off.addEventListener("click", function () {
     document.getElementById("overlay").style.display = "none";
     console.log("Goodbye");
-})  
+})
+
+
+
