dfdc162499f09c58be7110af34e737c72385ee5c
Spooked-Potato <arturjddias@hotmail.com>
2022-03-30 19:18:30 +0100

    last commit

--- JS/game.js
+++ JS/game.js
@@ -1,5 +1,7 @@
 const btn_on = document.querySelector(".button_top");
 const btn_off = document.querySelector(".btn__off");
+// const rows = document.querySelector(".row__grid");
+// const column = document.querySelector(".colum__grid");
 
 
 btn_on.addEventListener("click", function () {
@@ -24,16 +26,54 @@ for (let rowNumber = 0; rowNumber < rows; rowNumber++) {
         gridItem.classList.add('gridItem');
         gridItem.id = `${rowNumber}-${colNumber}`;
     }
-
 }
+
+
+// function MudarTamanho() { };
+// rows.addEventListener("input", MudarTamanho);
+// column.addEventListener("input", MudarTamanho);
+// MudarTamanho();
+
+
+
+
+
 
 
 submit.onclick = () => {
-
-    gridContainer.style.backgroundColor = snake__color.value;
-    gridContainer.style.backgroundColor = food__color.value;
     gridContainer.style.backgroundColor = grid__color.value;
+    snakeContainer.style.backgroundColor = snake__color.value;
+    foodContainer.style.backgroundColor = food__color.value;
 }
+
+
+
+let inputDirection = { x: 0, y: 0 }
+let lastInputDirection = { x: 0, y: 0 }
+
+window.addEventListener('keydown', e => {
+    switch (e.key) {
+        case 'ArrowUp':
+            if (lastInputDirection.y !== 0) break
+            inputDirection = { x: 0, y: -1 }
+            break
+
+        case 'ArrowDown':
+            if (lastInputDirection.y !== 0) break
+            inputDirection = { x: 0, y: 1 }
+            break
+
+        case 'ArrowLeft':
+            if (lastInputDirection.x !== 0) break
+            inputDirection = { x: -1, y: 0 }
+            break
+
+        case 'ArrowRight':
+            if (lastInputDirection.x !== 0) break
+            inputDirection = { x: 1, y: 0 }
+            break
+    }
+})
 
 btn_off.addEventListener("click", function () {
     document.getElementById("overlay").style.display = "none";
--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,17 @@
-// Wrap every letter in a span
-var textWrapper = document.querySelector('.ml9 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
 anime.timeline({ loop: true })
     .add({
-        targets: '.ml9 .letter',
-        scale: [0, 1],
-        duration: 1500,
-        elasticity: 600,
-        delay: (el, i) => 45 * (i + 1)
+        targets: '.word__container .word',
+        scale: [14, 1],
+        opacity: [0, 1],
+        easing: "easeOutCirc",
+        duration: 800,
+        delay: (el, i) => 800 * i
+
     }).add({
-        targets: '.ml9',
+        targets: '.word__container',
         opacity: 0,
         duration: 1000,
         easing: "easeOutExpo",
         delay: 1000
     });
-
 
