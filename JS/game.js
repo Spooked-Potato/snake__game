7abb1a61dd62c09a96f6764bf3380ec110123f9d
Spooked-Potato <arturjddias@hotmail.com>
2022-03-31 19:34:30 +0100

    Finished project, made full game where the user can change the color and size of the grid as well as the color of the snake and food, added a scored, each time the snake eats the food. remade some code, more of the JS part, added speed adjustability for the snake.

--- JS/game.js
+++ JS/game.js
@@ -1,83 +1,225 @@
+const gridSizeInput = document.getElementById("grid__size");
 const btn_on = document.querySelector(".button_top");
-const btn_off = document.querySelector(".btn__off");
-// const rows = document.querySelector(".row__grid");
-// const column = document.querySelector(".colum__grid");
-
 
 btn_on.addEventListener("click", function () {
     document.getElementById("overlay").style.display = "block";
 
 })
 
-var rows = 50;
-var columns = 50;
+let gameOver = false;
+let lastRenderTime = 0;
+let SNAKE_SPEED = 5;
+const EXPANSION_RATE = 1;
+let GRID_SIZE = gridSizeInput.value;
+const snakeBody = [
+    { x: 10, y: 11 }
 
-gridContainer = document.getElementById('gridContainer');
-for (let rowNumber = 0; rowNumber < rows; rowNumber++) {
+];
+let food = getRandomFoodPosition();
+let newSegments = 0;
+let score = 0;
+
+let root = document.querySelector(':root');
+const gameBoard = document.getElementById("game-board");
+const snakeEl = document.getElementsByClassName('snake');
+const snakeColorInput = document.getElementById("snake__color");
+const foodEl = document.getElementsByClassName('food');
+const foodColorInput = document.getElementById('food__color');
+const gridColorInput = document.getElementById('grid__color');
+const snakeSpeedInput = document.getElementById('snake__speed');
+const snakeScoreEl = document.querySelector(".score");
+
+let inputDirection = { x: 0, y: 0 };
+let lastInputDirection = { x: 0, y: 0 };
+
+function gridSize() {
+    GRID_SIZE = gridSizeInput.value
+    gameBoard.style.gridTemplateColumns = `repeat(${GRID_SIZE}, 1fr)`;
+    gameBoard.style.gridTemplateRows = `repeat(${GRID_SIZE}, 1fr)`;
+}
+
+function snakeSpeed() {
+    SNAKE_SPEED = snakeSpeedInput.value;
+}
+
+gridSizeInput.addEventListener("input", () => {
+    gridSize();
+})
+gridSize();
+
+snakeSpeedInput.addEventListener("input", () => {
+    snakeSpeed();
+})
+snakeSpeed();
+
+snakeColorInput.addEventListener("input", () => {
+    root.style.setProperty('--snake__color', snakeColorInput.value);
+})
+root.style.setProperty('--snake__color', snakeColorInput.value);
+
+foodColorInput.addEventListener("input", () => {
+    root.style.setProperty('--food__color', foodColorInput.value);
+})
+root.style.setProperty('--food__color', foodColorInput.value);
+
+gridColorInput.addEventListener("input", () => {
+    root.style.setProperty('--grid__color', gridColorInput.value);
+})
+root.style.setProperty('--grid__color', gridColorInput.value);
 
 
-    var row = document.createElement('div');
-    row.classList.add('row');
-    gridContainer.appendChild(row);
-
-    for (let colNumber = 0; colNumber < columns; colNumber++) {
-        var gridItem = document.createElement('div');
-        row.appendChild(gridItem);
-        gridItem.classList.add('gridItem');
-        gridItem.id = `${rowNumber}-${colNumber}`;
+function main(currentTime) {
+    if (gameOver) {
+        if (confirm(`GAME OVER\n\nYou lost.\nYour score was ${score} Press ok to restart`)) {
+            window.location.reload();
+        }
+        return;
     }
+
+    window.requestAnimationFrame(main);
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+    lastRenderTime = currentTime;
+
+    update();
+    draw();
 }
 
+window.requestAnimationFrame(main);
 
-// function MudarTamanho() { };
-// rows.addEventListener("input", MudarTamanho);
-// column.addEventListener("input", MudarTamanho);
-// MudarTamanho();
-
-
-
-
-
-
-
-submit.onclick = () => {
-    gridContainer.style.backgroundColor = grid__color.value;
-    snakeContainer.style.backgroundColor = snake__color.value;
-    foodContainer.style.backgroundColor = food__color.value;
-}
-
-
-
-let inputDirection = { x: 0, y: 0 }
-let lastInputDirection = { x: 0, y: 0 }
-
-window.addEventListener('keydown', e => {
+window.addEventListener("keydown", e => {
     switch (e.key) {
         case 'ArrowUp':
+        case 'w':
             if (lastInputDirection.y !== 0) break
             inputDirection = { x: 0, y: -1 }
-            break
-
+            break;
         case 'ArrowDown':
+        case 's':
             if (lastInputDirection.y !== 0) break
             inputDirection = { x: 0, y: 1 }
-            break
-
+            break;
         case 'ArrowLeft':
+        case 'a':
             if (lastInputDirection.x !== 0) break
             inputDirection = { x: -1, y: 0 }
-            break
-
+            break;
         case 'ArrowRight':
+        case 'd':
             if (lastInputDirection.x !== 0) break
-            inputDirection = { x: 1, y: 0 }
-            break
+            inputDirection = { x: 1, y: -0 }
+            break;
     }
 })
 
-btn_off.addEventListener("click", function () {
-    document.getElementById("overlay").style.display = "none";
-})
+function getInputDirection() {
+    lastInputDirection = inputDirection;
+    return inputDirection;
+}
 
+function updateSnake() {
+    addSegments();
+    const inputDirection = getInputDirection();
+    for (let i = snakeBody.length - 2; i >= 0; i--) {
+        snakeBody[i + 1] = { ...snakeBody[i] };
+    }
 
+    snakeBody[0].x += inputDirection.x;
+    snakeBody[0].y += inputDirection.y;
+}
 
+function drawSnake(gameBoard) {
+    snakeBody.forEach(element => {
+        const snakeElement = document.createElement('div');
+        snakeElement.style.gridRowStart = element.y;
+        snakeElement.style.gridColumnStart = element.x;
+        snakeElement.classList.add("snake");
+        gameBoard.appendChild(snakeElement);
+    })
+}
+
+function expandSnake(amount) {
+    newSegments += amount;
+}
+
+function onSnake(position, { ignoreHead = false } = {}) {
+    return snakeBody.some((segment, index) => {
+        if (ignoreHead && index === 0) return false
+        return equalPositions(segment, position)
+    })
+}
+
+function getSnakeHead() {
+    return snakeBody[0];
+}
+
+function snakeIntersection() {
+    return onSnake(snakeBody[0], { ignoreHead: true });
+}
+
+function equalPositions(pos1, pos2) {
+    return pos1.x === pos2.x && pos1.y === pos2.y;
+}
+
+function addSegments() {
+    for (let i = 0; i < newSegments; i++) {
+        snakeBody.push({ ...snakeBody[snakeBody.length - 1] });
+    }
+
+    newSegments = 0;
+}
+
+function updateFood() {
+    if (onSnake(food)) {
+        expandSnake(EXPANSION_RATE);
+        score++;
+        snakeScoreEl.textContent = "Score: " + score;
+        food = getRandomFoodPosition();
+    }
+}
+
+function drawFood() {
+    const foodElement = document.createElement('div');
+    foodElement.style.gridRowStart = food.y;
+    foodElement.style.gridColumnStart = food.x;
+    foodElement.classList.add("food");
+    gameBoard.appendChild(foodElement);
+}
+
+function getRandomFoodPosition() {
+    let newFoodPosition;
+    while (newFoodPosition == null || onSnake(newFoodPosition)) {
+        newFoodPosition = randomGridPostion();
+    }
+    return newFoodPosition;
+}
+
+function randomGridPostion() {
+    return {
+        x: Math.floor(Math.random() * GRID_SIZE) + 1,
+        y: Math.floor(Math.random() * GRID_SIZE) + 1
+    }
+}
+
+function update() {
+    updateSnake();
+    updateFood();
+    checkForDeath();
+}
+
+function draw() {
+    gameBoard.innerHTML = '';
+    drawSnake(gameBoard);
+    drawFood(gameBoard);
+}
+
+function outsideGrid(position) {
+    return (
+        position.x < 1 || position.x > GRID_SIZE ||
+        position.y < 1 || position.y > GRID_SIZE
+    )
+}
+
+function checkForDeath() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
+}
---
+++ README.txt
@@ -0,0 +1,7 @@
+Hi there, thank you for checking-out my code.
+Few things to know, this is a simple snake-game like the old nokia phone.
+The controls are the arrowkeys or "wasd" keys
+If you touch the walls you will die and have to restar all over.
+you scored is added +1.
+
+Have fun :)
